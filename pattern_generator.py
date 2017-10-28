5de26ae09baa6d0ba137a8e2d962a7050ef65a67
num1r0 <persianov.s@gmail.com>
2017-10-28 08:14:30 +0100

    get_buffer_size() function added

--- pattern_generator.py
+++ pattern_generator.py
@@ -2,7 +2,7 @@
 Simple random pattern generator script
 
 TO DO:
-    - Add pattern search function (used for EIP detection)
+    - Add buffer size computation function (used for EIP detection)
 """
 
 import sys
@@ -21,10 +21,31 @@ def pattern_generator(length):
                 else:
                     return pattern[:length]
 
+def get_buffer_size(address):
+    """
+    Gets little endian memory address and finds buffer size
+    """
+    if address[:2] == "0x":
+        address = address[2:]
+        ascii_block = address.decode("hex")
+        pattern = ""
+        for upper_char in ascii_uppercase:
+            for lower_char in ascii_lowercase:
+                for digit in digits:
+                    pattern += upper_char + lower_char + digit
+                    buffer_size = pattern.find(ascii_block)
+                    if buffer_size > -1:
+                        return buffer_size
+    else:
+        print "Little Endian memory address expected (ex.: 0x41424344)"
+        
+
 def main():
-    length = sys.argv[1]
-    if length.isdigit():
-        print pattern_generator(int(length))
+    user_input = sys.argv[1]
+    if user_input.isdigit():
+        print pattern_generator(int(user_input))
+    else:
+        print get_buffer_size(str(user_input))
 
 if __name__ == "__main__":
     main()
